fac0219b8bd8f2eb10960c3fa5a33faf82514704
Ranika Williams <rnkprice07@gmail.com>
2019-08-20 16:02:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
+    t = Time.now
     
-    
-    if time < 12
+    if t < 12
     resp.write "Good Morning!"
     else
     resp.write "Good Afternoon!"
